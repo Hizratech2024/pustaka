1b3e9dd071535971891010d631ff832b9c8d30e1
kaduk7 <zulakmal24@gmail.com>
2024-05-31 22:59:02 +0700

    tttt

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -68,61 +68,76 @@ const Login = () => {
   };
 
   return (
-    <div className="authincation h-100 common-img-bg">
+    <div className="authincation h-100 commonImgBg">
       <div className="container-fluid h-100">
         <div className="row h-100">
-          <div className="col-lg-6 col-md-12 col-sm-12 mx-auto align-self-center">
+        <div className="col-lg-6 col-md-12 col-sm-12 mx-auto align-self-center">
             <div className="login-form">
               <div className="text-center">
                 <img src="/tema/images/tech.png" width='400' height='200' className="mt-5 mb-3" alt="" />
-                <h3 className="title" style={{ color:'honeydew' }}>i-Tech Komputer</h3>
-                <p style={{ color:'honeydew' }}>Selamat Datang di Website i-Tech Komputer</p>
+                {/* <h3 className="title" style={{ color: 'bisque' }}>Selamat Datang  di Website I-Tech Komputer</h3> */}
+                <p style={{ color: 'bisque' }}>Selamat Datang  di Website I-Tech Komputer</p>
               </div>
-                <form onSubmit={handleSubmit}>
-                  <div className="mb-4">
-                    {/* <label className="mb-1" style={{ color:'black',fontSize:18 }}>Username</label> */}
-                    <input
-                      type="email"
-                      className="form-control form-control"
-                      onChange={(e) => setUsernama(e.target.value)}
-                      placeholder="Username"
-                    />
-                  </div>
-                  <div className="mb-4 position-relative">
-                  {/* <label className="mb-1" style={{ color:'black',fontSize:18 }}>Password</label> */}
-                    <input
-                      required
-                      value={passwordText}
-                      type={st ? "text" : "password"}
-                      className="form-control form-control"
-                      onChange={(e) => setPasswordText(e.target.value)}
-                      placeholder="Password"
+              <form onSubmit={handleSubmit}>
+                <div className="mb-4">
+                  {/* <label className="mb-1" style={{ color:'#ec8200',fontSize:18,fontFamily:"initial" }}>Username</label> */}
+                  <input
+                    type="email"
+                    className="form-control form-control"
+                    onChange={(e) => setUsernama(e.target.value)}
+                    placeholder="Username"
+                    style={{backgroundColor:"antiquewhite"}}
+                  />
+                </div>
+                <div className="mb-4 position-relative">
+                  {/* <label className="mb-1" style={{ color:'green',fontSize:18,fontFamily:"initial" }}>Password</label> */}
+                  <input
+                    required
+                    value={passwordText}
+                    type={st ? "text" : "password"}
+                    className="form-control form-control"
+                    onChange={(e) => setPasswordText(e.target.value)}
+                    placeholder="Password"
+                    style={{backgroundColor:"antiquewhite"}}
+                  />
 
-                    />
-
-                    <span className="show-pass eye">
-                      {st ?
-                        <a onClick={() => setSt(!st)} className="" >
-                          <i className="mdi mdi-eye" />
-                        </a>
-                        :
-                        <a onClick={() => setSt(!st)} className="" >
-                          <i className="mdi mdi-eye-off" />
-                        </a>
-                      }
-                    </span>
-                  </div>
-                  <div className="text-center mb-4">
-                    <button type="submit" className="btn btn-info  btn-block">
-                      Sign In
-                    </button>
-                  </div>
+                  <span className="show-pass eye">
+                    {st ?
+                      <a onClick={() => setSt(!st)} className="" >
+                        <i className="mdi mdi-eye" />
+                      </a>
+                      :
+                      <a onClick={() => setSt(!st)} className="" >
+                        <i className="mdi mdi-eye-off" />
+                      </a>
+                    }
+                  </span>
+                </div>
+                <div className="text-center mb-4">
+                  <button type="submit" className="btn btn-info  btn-block">
+                    LOGIN
+                  </button>
+                </div>
 
 
-                </form>
+              </form>
+            </div>
+          </div>
+          {/* <div className="col-xl-6 col-lg-6">
+            <div className="pages-left h-100">
+              <div className="login-content">
+                <p>
+
+                </p>
+              </div>
+              <div className="login-media text-center">
+                <img src="/tema/images/z.png" alt="" />
+              </div>
+              <div className="login-content">
+
               </div>
             </div>
-
+          </div> */}
         </div>
       </div>
     </div>
