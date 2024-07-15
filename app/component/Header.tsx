603e7ceea58a687453fd643b4cd296b31c7b8e89
kaduk24 <izull.akmal@gmail.com>
2024-07-15 15:57:08 +0700

    fsdfsdf

--- app/component/Header.tsx
+++ app/component/Header.tsx
@@ -8,8 +8,8 @@ const xxx=session.data
         <div>
             <div className="nav-header">
             <a href="/" className="brand-logo">
-                    <img alt="" width="55" height="55" className="logo-abbr" src="/tema/images/lgo.png" />
-                    <img alt="" width="220" height="51" className="brand-title" src="/tema/images/tulisan.png" />
+                    <img alt="" width="55" height="55" className="logo-abbr" src="/tema/images/logoatas.png" />
+                    <img alt="" width="220" height="51" className="brand-title" src="/tema/images/tull.png" />
                 </a>
                 <div className="nav-control">
                     <div className="hamburger">
