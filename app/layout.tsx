a5e4417ef3a9547f781f2109c5e0a68da00db118
kaduk24 <izull.akmal@gmail.com>
2024-08-26 14:05:51 +0700

    dsdsdsds

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,11 +11,12 @@ export default function RootLayout({
   const pathname = usePathname()
   return (
     <html lang="id">
-      {/* <head>
+      <head>
         <meta charSet="utf-8" />
-        <title>I-Tech Komputer</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        {/* <title>I-Tech Komputer</title> */}
         
-      </head> */}
+      </head>
 
       <body>
         <Provider>
