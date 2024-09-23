8b744e2266120e526b501bc93cbdeea3432e8e97
Hizratech2024 <hizratechkonsultanservices@gmail.com>
2024-09-23 13:23:53 +0700

    2222

--- app/api/mobile/kategori/route.tsx
+++ app/api/mobile/kategori/route.tsx
@@ -1,23 +1,30 @@
-import { NextResponse } from "next/server"
+import { NextRequest, NextResponse } from "next/server"
 import { PrismaClient, KategoriTb } from "@prisma/client"
+import { getToken } from "next-auth/jwt";
 
 const prisma = new PrismaClient()
 
-export const POST = async (request: Request) => {
+export const POST = async (request: NextRequest) => {
+    const token = await getToken({
+        req: request,
+        secret: process.env.NEXTAUTH_SECRET,
+    });
 
+    const sekolahId = Number(token?.sekolahId);
     const body = await request.json();
-    const kategori = await prisma.kategoriTb.create({    
+    await prisma.kategoriTb.create({
         data: {
             nama: body.nama,
+            sekolahId: sekolahId
         }
     })
-    return NextResponse.json( {pesan: 'berhasil',status:200})
+    return NextResponse.json({ pesan: 'berhasil', status: 200 })
 }
 
 export const GET = async () => {
     const kategori = await prisma.kategoriTb.findMany({
-        orderBy:{
-            nama:'asc'
+        orderBy: {
+            nama: 'asc'
         }
     });
     return NextResponse.json(kategori, { status: 200 })
